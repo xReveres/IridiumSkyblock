d6e775b18453fdfdc4d71cce2173bd321ba467db
Peaches_MLG <PeachesMLG100@gmail.com>
2019-08-23 16:02:17 +0100

    quick bug fix

--- src/main/java/com/peaches/epicskyblock/listeners/onClick.java
+++ src/main/java/com/peaches/epicskyblock/listeners/onClick.java
@@ -13,22 +13,24 @@ public class onClick implements Listener {
     public void onClick(PlayerInteractEvent e) {
         try {
             User u = User.getUser(e.getPlayer());
-            if (e.getClickedBlock().getLocation().getWorld().equals(EpicSkyblock.getIslandManager().getWorld())) {
-                Island island = u.getIsland();
-                if (island != null) {
-                    if ((e.getClickedBlock().getX() > island.getPos1().getX() && e.getClickedBlock().getX() <= island.getPos2().getX()) && (e.getClickedBlock().getZ() > island.getPos1().getZ() && e.getClickedBlock().getZ() <= island.getPos2().getZ())) {
-                        // Block is in players island
-                        if (!u.bypassing && !u.getIsland().getPermissions(u.role).interact) {
-                            e.setCancelled(true);
+            if (e.getClickedBlock() != null) {
+                if (e.getClickedBlock().getLocation().getWorld().equals(EpicSkyblock.getIslandManager().getWorld())) {
+                    Island island = u.getIsland();
+                    if (island != null) {
+                        if ((e.getClickedBlock().getX() > island.getPos1().getX() && e.getClickedBlock().getX() <= island.getPos2().getX()) && (e.getClickedBlock().getZ() > island.getPos1().getZ() && e.getClickedBlock().getZ() <= island.getPos2().getZ())) {
+                            // Block is in players island
+                            if (!u.bypassing && !u.getIsland().getPermissions(u.role).interact) {
+                                e.setCancelled(true);
+                            }
+                        } else {
+                            if (!u.bypassing) {
+                                e.setCancelled(true);
+                            }
                         }
                     } else {
                         if (!u.bypassing) {
                             e.setCancelled(true);
                         }
-                    }
-                } else {
-                    if (!u.bypassing) {
-                        e.setCancelled(true);
                     }
                 }
             }
