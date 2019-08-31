ebc89b62a86da92117004b9674622db4f28e16fd
Peaches_MLG <PeachesMLG100@gmail.com>
2019-08-31 12:05:16 +0100

    Forgot to commit this file

--- src/main/java/com/peaches/epicskyblock/EpicSkyblock.java
+++ src/main/java/com/peaches/epicskyblock/EpicSkyblock.java
@@ -6,25 +6,15 @@ import com.peaches.epicskyblock.gui.TopGUI;
 import com.peaches.epicskyblock.listeners.*;
 import com.peaches.epicskyblock.placeholders.ClipPlaceholderAPIManager;
 import com.peaches.epicskyblock.serializer.Persist;
-import io.sentry.SentryClient;
-import io.sentry.SentryClientFactory;
-import io.sentry.event.Event;
-import io.sentry.event.EventBuilder;
 import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.net.InetAddress;
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Set;
 
 public class EpicSkyblock extends JavaPlugin {
 
