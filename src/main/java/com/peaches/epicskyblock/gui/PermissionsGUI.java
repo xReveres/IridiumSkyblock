0f76b402b4434d2bb7f2011e18efa18f6f83582f
Peaches_MLG <PeachesMLG100@gmail.com>
2019-09-03 13:09:47 +0100

    Forgot to commit this

---
+++ src/main/java/com/peaches/epicskyblock/gui/PermissionsGUI.java
@@ -0,0 +1,45 @@
+package com.peaches.epicskyblock.gui;
+
+import com.peaches.epicskyblock.*;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
+
+public class PermissionsGUI implements Listener {
+
+    public Inventory inventory;
+
+    public HashMap<Integer, Roles> roles;
+
+    public int islandid;
+
+    public PermissionsGUI(Island island) {
+        islandid = island.getId();
+        this.inventory = Bukkit.createInventory(null, 27, Utils.color(EpicSkyblock.getConfiguration().BorderColorGUITitle));
+        for (int i = 0; i < 27; i++) {
+            inventory.setItem(i, Utils.makeItem(Material.STAINED_GLASS_PANE, 1, 7, " "));
+        }
+        roles = new HashMap<>();
+        for (int i = 1; i < Roles.Owner.getRank(); i++) {
+            roles.put(10 + i, Roles.getViaRank(i));
+        }
+        EpicSkyblock.getInstance().registerListeners(this);
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent e) {
+        if (e.getInventory().equals(inventory)) {
+            e.setCancelled(true);
+            if (roles.containsKey(e.getSlot())) {
+                
+            }
+        }
+    }
+}
