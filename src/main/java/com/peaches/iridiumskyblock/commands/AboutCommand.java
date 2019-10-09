938bb9b26c007db60ac8e31fd8c05fed830f4f54
DaRiches <zanecarson808@gmail.com>
2019-10-09 06:58:37 -1000

    name change 3

--- src/main/java/com/peaches/iridiumskyblock/commands/AboutCommand.java
+++ src/main/java/com/peaches/iridiumskyblock/commands/AboutCommand.java
@@ -15,7 +15,7 @@ public class AboutCommand extends Command {
 
     @Override
     public void execute(CommandSender sender, String[] args) {
-        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8Plugin Name: &7EpicSkyblock"));
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8Plugin Name: &7IridiumSkyblock"));
         sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8Plugin Version: &7" + IridiumSkyblock.getInstance().getDescription().getVersion()));
         sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8Plugin Author: &7Peaches_MLG"));
     }
