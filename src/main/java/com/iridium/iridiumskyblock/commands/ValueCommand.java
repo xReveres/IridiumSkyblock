6e32273fe4bad4e3ff7e85091c75f20350d25c00
Peaches_MLG <PeachesMLG100@gmail.com>
2019-11-16 14:11:07 +0000

    Added ability to show island value of other players

--- src/main/java/com/iridium/iridiumskyblock/commands/ValueCommand.java
+++ src/main/java/com/iridium/iridiumskyblock/commands/ValueCommand.java
@@ -1,8 +1,11 @@
 package com.iridium.iridiumskyblock.commands;
 
 import com.iridium.iridiumskyblock.IridiumSkyblock;
+import com.iridium.iridiumskyblock.Island;
 import com.iridium.iridiumskyblock.User;
 import com.iridium.iridiumskyblock.Utils;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -19,8 +22,15 @@ public class ValueCommand extends Command {
     public void execute(CommandSender sender, String[] args) {
         Player p = (Player) sender;
         User user = User.getUser(p);
-        if (user.getIsland() != null) {
-            p.sendMessage(Utils.color(IridiumSkyblock.getMessages().islandValue.replace("%value%", user.getIsland().getValue() + "").replace("%rank%", Utils.getIslandRank(user.getIsland()) + "").replace("%prefix%", IridiumSkyblock.getConfiguration().prefix)));
+        Island island;
+        if (args.length == 2) {
+            OfflinePlayer player = Bukkit.getOfflinePlayer(args[1]);
+            island = User.getUser(player).getIsland();
+        } else {
+            island = user.getIsland();
+        }
+        if (island != null) {
+            p.sendMessage(Utils.color(IridiumSkyblock.getMessages().islandValue.replace("%value%", island.getValue() + "").replace("%rank%", Utils.getIslandRank(island) + "").replace("%prefix%", IridiumSkyblock.getConfiguration().prefix)));
         } else {
             p.sendMessage(Utils.color(IridiumSkyblock.getMessages().noIsland.replace("%prefix%", IridiumSkyblock.getConfiguration().prefix)));
         }
