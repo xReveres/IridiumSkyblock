930fc037b1b915240959aacdd6c7e9f4c6dff6fd
Peaches_MLG <PeachesMLG100@gmail.com>
2019-11-16 20:48:37 +0000

    Removed Multiverse Support

--- src/main/java/com/iridium/iridiumskyblock/IridiumSkyblock.java
+++ src/main/java/com/iridium/iridiumskyblock/IridiumSkyblock.java
@@ -75,7 +75,6 @@ public class IridiumSkyblock extends JavaPlugin {
 
                 if (Bukkit.getPluginManager().getPlugin("Vault") != null) new Vault();
                 if (Bukkit.getPluginManager().isPluginEnabled("WildStacker")) new Wildstacker();
-                if (Bukkit.getPluginManager().getPlugin("Multiverse-Core") != null) registerMultiverse();
 
                 // Call it as a delayed task to wait for the server to properly load first
                 Bukkit.getScheduler().scheduleSyncDelayedTask(IridiumSkyblock.getInstance(), IridiumSkyblock.getInstance()::islandValueManager);
@@ -129,16 +128,6 @@ public class IridiumSkyblock extends JavaPlugin {
             getLogger().info("-------------------------------");
         } catch (Exception e) {
             sendErrorMessage(e);
-        }
-    }
-
-    private void registerMultiverse() {
-        Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "mv import " + getIslandManager().getWorld().getName() + " normal -g " + getName());
-        Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "mv modify set generator " + getName() + " " + getIslandManager().getWorld().getName());
-
-        if (IridiumSkyblock.getConfiguration().netherIslands) {
-            Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "mv import " + getIslandManager().getNetherWorld().getName() + " nether -g " + getName());
-            Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "mv modify set generator " + getName() + " " + getIslandManager().getNetherWorld().getName());
         }
     }
 
