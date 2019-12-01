65d714a002bedfffd1534b8968223d97b4482500
Peaches_MLG <PeachesMLG100@gmail.com>
2019-12-01 13:46:36 +0000

    Fixed Typo

--- src/main/java/com/iridium/iridiumskyblock/configs/Config.java
+++ src/main/java/com/iridium/iridiumskyblock/configs/Config.java
@@ -45,11 +45,19 @@ public class Config {
             }
         }
     }};
-    public HashMap<Material, Integer> blockvalue = new HashMap<Material, Integer>() {{
-        put(Material.DIAMOND_BLOCK, 10);
-        put(Material.EMERALD_BLOCK, 20);
-        put(Material.BEACON, 100);
+    public HashMap<Integer, Integer> islandTopSlots = new HashMap<Integer, Integer>() {{
+        put(1, 4);
+        put(2, 12);
+        put(3, 14);
+        put(4, 19);
+        put(5, 20);
+        put(6, 21);
+        put(7, 22);
+        put(8, 23);
+        put(9, 24);
+        put(10, 25);
     }};
+    
+    public HashMap<Material, Integer> blockvalue = null;
     public HashMap<String, Integer> spawnervalue = null;
-    public HashMap<Integer, Integer> islandTopSlots = null;
 }
