be8c12ff903d03d1288da85ae2a4fa4fc5d16c36
Peaches_MLG <PeachesMLG100@gmail.com>
2019-12-20 13:21:47 +0000

    Fixed issues with 1.8 compatibility

--- src/main/java/com/iridium/iridiumskyblock/listeners/onEntityPickupItem.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/onEntityPickupItem.java
@@ -4,19 +4,17 @@ import com.iridium.iridiumskyblock.IridiumSkyblock;
 import com.iridium.iridiumskyblock.Island;
 import com.iridium.iridiumskyblock.Role;
 import com.iridium.iridiumskyblock.User;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
 
 public class onEntityPickupItem implements Listener {
 
     @EventHandler
-    public void onEntityPickupItem(EntityPickupItemEvent e) {
+    public void onEntityPickupItem(PlayerPickupItemEvent e) {
         Island island = IridiumSkyblock.getIslandManager().getIslandViaLocation(e.getItem().getLocation());
-        if (island != null && e.getEntity() instanceof Player) {
-            User u = User.getUser((OfflinePlayer) e.getEntity());
+        if (island != null) {
+            User u = User.getUser(e.getPlayer());
             if ((!island.getPermissions((u.islandID == island.getId() || island.isCoop(u.getIsland())) ? (island.isCoop(u.getIsland()) ? Role.Member : u.getRole()) : Role.Visitor).pickupItems) && !u.bypassing)
                 e.setCancelled(true);
         }
