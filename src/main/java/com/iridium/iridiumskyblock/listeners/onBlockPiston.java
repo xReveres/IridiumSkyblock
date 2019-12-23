411ddc8ac194477a37ede482d09848a475fd0b9f
Peaches_MLG <PeachesMLG100@gmail.com>
2019-12-23 15:11:41 +0000

    Fixed NPE on onBlockPiston

--- src/main/java/com/iridium/iridiumskyblock/listeners/onBlockPiston.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/onBlockPiston.java
@@ -14,30 +14,32 @@ public class onBlockPiston implements Listener {
     @EventHandler
     public void onBlockPistonExtend(BlockPistonExtendEvent e) {
         Island island = IridiumSkyblock.getIslandManager().getIslandViaLocation(e.getBlock().getLocation());
-        for (Block b : e.getBlocks()) {
-            Location loc = b.getLocation();
-            switch (e.getDirection()) {
-                case NORTH:
-                    loc.add(0, 0, -1);
-                    break;
-                case EAST:
-                    loc.add(1, 0, 0);
-                    break;
-                case SOUTH:
-                    loc.add(0, 0, 1);
-                    break;
-                case WEST:
-                    loc.add(-1, 0, 0);
-                    break;
-                case UP:
-                    loc.add(0, 1, 0);
-                    break;
-                case DOWN:
-                    loc.add(0, -1, 0);
-                    break;
-            }
-            if (!island.isInIsland(loc)) {
-                e.setCancelled(true);
+        if (island != null) {
+            for (Block b : e.getBlocks()) {
+                Location loc = b.getLocation();
+                switch (e.getDirection()) {
+                    case NORTH:
+                        loc.add(0, 0, -1);
+                        break;
+                    case EAST:
+                        loc.add(1, 0, 0);
+                        break;
+                    case SOUTH:
+                        loc.add(0, 0, 1);
+                        break;
+                    case WEST:
+                        loc.add(-1, 0, 0);
+                        break;
+                    case UP:
+                        loc.add(0, 1, 0);
+                        break;
+                    case DOWN:
+                        loc.add(0, -1, 0);
+                        break;
+                }
+                if (!island.isInIsland(loc)) {
+                    e.setCancelled(true);
+                }
             }
         }
     }
@@ -46,9 +48,11 @@ public class onBlockPiston implements Listener {
     @EventHandler
     public void onBlockPistonReact(BlockPistonRetractEvent e) {
         Island island = IridiumSkyblock.getIslandManager().getIslandViaLocation(e.getBlock().getLocation());
-        for (Block b : e.getBlocks()) {
-            if (!island.isInIsland(b.getLocation())) {
-                e.setCancelled(true);
+        if (island != null) {
+            for (Block b : e.getBlocks()) {
+                if (!island.isInIsland(b.getLocation())) {
+                    e.setCancelled(true);
+                }
             }
         }
     }
