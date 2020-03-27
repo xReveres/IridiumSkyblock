7177cb4d312e718c179a1e54b2139ccf6c523b6a
Peaches_MLG <PeachesMLG100@gmail.com>
2020-03-27 14:41:20 +0000

    Fixed issues with people being able to push tntminecarts into others islands

--- src/main/java/com/iridium/iridiumskyblock/listeners/onEntitySpawn.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/onEntitySpawn.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.vehicle.VehicleCreateEvent;
 
 public class onEntitySpawn implements Listener {
 
@@ -15,6 +16,14 @@ public class onEntitySpawn implements Listener {
         if (IridiumSkyblock.getConfiguration().blockedEntities.contains(e.getEntityType()) && (e.getEntity().getLocation().getWorld().equals(IridiumSkyblock.getIslandManager().getNetherWorld()) || e.getEntity().getLocation().getWorld().equals(IridiumSkyblock.getIslandManager().getWorld()))) {
             IridiumSkyblock.getInstance().entities.put(e.getEntity().getUniqueId(), IridiumSkyblock.getIslandManager().getIslandViaLocation(e.getEntity().getLocation()));
             monitorEntity(e.getEntity());
+        }
+    }
+
+    @EventHandler
+    public void onVehicleSpawn(VehicleCreateEvent e) {
+        if (IridiumSkyblock.getConfiguration().blockedEntities.contains(e.getVehicle().getType()) && (e.getVehicle().getLocation().getWorld().equals(IridiumSkyblock.getIslandManager().getNetherWorld()) || e.getVehicle().getLocation().getWorld().equals(IridiumSkyblock.getIslandManager().getWorld()))) {
+            IridiumSkyblock.getInstance().entities.put(e.getVehicle().getUniqueId(), IridiumSkyblock.getIslandManager().getIslandViaLocation(e.getVehicle().getLocation()));
+            monitorEntity(e.getVehicle());
         }
     }
 
