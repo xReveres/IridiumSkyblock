5e6cdfa601c93676abf6220b329f9a5d0cb48cb4
Peaches_MLG <PeachesMLG100@gmail.com>
2020-04-06 23:44:32 +0100

    Redesigned Netherportal system

--- src/main/java/com/iridium/iridiumskyblock/listeners/onPlayerPortal.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/onPlayerPortal.java
@@ -7,24 +7,30 @@ import com.iridium.iridiumskyblock.User;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerPortalEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
 public class onPlayerPortal implements Listener {
 
     @EventHandler
     public void onPlayerPortal(PlayerPortalEvent e) {
-        if (IridiumSkyblock.getConfiguration().netherIslands) {
-            User u = User.getUser(e.getPlayer());
-            Island island = IridiumSkyblock.getIslandManager().getIslandViaLocation(e.getFrom());
-            if (island != null) {
-                if (e.getFrom().getWorld().equals(IridiumSkyblock.getIslandManager().getWorld())) {
-                    e.setCancelled(true);
-                    if (island.getPermissions((u.islandID == island.getId() || island.isCoop(u.getIsland())) ? (island.isCoop(u.getIsland()) ? Role.Member : u.getRole()) : Role.Visitor).useNetherPortal || u.bypassing)
-                        island.teleportNetherHome(e.getPlayer());
-                } else if (e.getFrom().getWorld().equals(IridiumSkyblock.getIslandManager().getNetherWorld())) {
-                    e.setCancelled(true);
-                    if (island.getPermissions((u.islandID == island.getId() || island.isCoop(u.getIsland())) ? (island.isCoop(u.getIsland()) ? Role.Member : u.getRole()) : Role.Visitor).useNetherPortal || u.bypassing)
-                        island.teleportHome(e.getPlayer());
+        if (e.getCause().equals(PlayerTeleportEvent.TeleportCause.NETHER_PORTAL)) {
+            if (IridiumSkyblock.getConfiguration().netherIslands) {
+                User u = User.getUser(e.getPlayer());
+                Island island = IridiumSkyblock.getIslandManager().getIslandViaLocation(e.getFrom());
+                if (island != null) {
+                    if (island.getPermissions((u.islandID == island.getId() || island.isCoop(u.getIsland())) ? (island.isCoop(u.getIsland()) ? Role.Member : u.getRole()) : Role.Visitor).useNetherPortal || u.bypassing) {
+                        e.setCanCreatePortal(true);
+                        if (e.getFrom().getWorld().equals(IridiumSkyblock.getIslandManager().getWorld())) {
+                            e.setTo(island.getNetherhome());
+                        } else {
+                            e.setTo(island.getHome());
+                        }
+                    } else {
+                        e.setCancelled(true);
+                    }
                 }
+            } else {
+                e.setCancelled(true);
             }
         }
     }
