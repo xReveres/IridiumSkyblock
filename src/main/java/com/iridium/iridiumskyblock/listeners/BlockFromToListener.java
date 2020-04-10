64a813a314840e0d780e0208d471a4357e96c147
Peaches_MLG <PeachesMLG100@gmail.com>
2020-04-10 23:45:25 +0100

    Fixed issue with generators

--- src/main/java/com/iridium/iridiumskyblock/listeners/BlockFromToListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/BlockFromToListener.java
@@ -1,10 +1,6 @@
 package com.iridium.iridiumskyblock.listeners;
 
-import com.iridium.iridiumskyblock.IridiumSkyblock;
-import com.iridium.iridiumskyblock.Island;
-import com.iridium.iridiumskyblock.IslandManager;
-import com.iridium.iridiumskyblock.Utils;
-import com.iridium.iridiumskyblock.XMaterial;
+import com.iridium.iridiumskyblock.*;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -27,14 +23,14 @@ public class BlockFromToListener implements Listener {
         try {
             final Block block = event.getBlock();
             final Material material = block.getType();
+            final Block toBlock = event.getToBlock();
+            final Location toLocation = toBlock.getLocation();
 
             if (material.equals(Material.WATER) || material.equals(Material.LAVA)) {
                 final Location location = block.getLocation();
                 final IslandManager islandManager = IridiumSkyblock.getIslandManager();
                 final Island island = islandManager.getIslandViaLocation(location);
                 if (island != null) {
-                    final Block toBlock = event.getToBlock();
-                    final Location toLocation = toBlock.getLocation();
                     final Island toIsland = islandManager.getIslandViaLocation(toLocation);
                     if (island != toIsland)
                         event.setCancelled(true);
@@ -43,8 +39,6 @@ public class BlockFromToListener implements Listener {
 
             if (!IridiumSkyblock.getUpgrades().oresUpgrade.enabled) return;
 
-            if (!(material.equals(Material.COBBLESTONE) || material.equals(Material.STONE))) return;
-
             final BlockFace face = event.getFace();
             if (face == BlockFace.DOWN) return;
 
@@ -52,20 +46,18 @@ public class BlockFromToListener implements Listener {
             final World world = location.getWorld();
             if (world == null) return;
 
-            if (!isSurroundedByWater(location))
+            if (!isSurroundedByWater(toLocation))
                 return;
 
             final IslandManager islandManager = IridiumSkyblock.getIslandManager();
             final Island island = islandManager.getIslandViaLocation(location);
             if (island == null) return;
 
-            event.setCancelled(true);
-
             final World islandWorld = islandManager.getWorld();
             if (islandWorld == null) return;
 
             final World islandNetherWorld = islandManager.getNetherWorld();
-            if (islandNetherWorld == null) return;
+            if (islandNetherWorld == null && IridiumSkyblock.getConfiguration().netherIslands) return;
 
             final String worldName = world.getName();
             final int oreLevel = island.getOreLevel();
@@ -73,23 +65,26 @@ public class BlockFromToListener implements Listener {
             List<String> islandOreUpgrades;
             if (worldName.equals(islandWorld.getName()))
                 islandOreUpgrades = IridiumSkyblock.oreUpgradeCache.get(oreLevel);
-            else if (worldName.equals(islandNetherWorld.getName()))
+            else if (IridiumSkyblock.getConfiguration().netherIslands && worldName.equals(islandNetherWorld.getName()))
                 islandOreUpgrades = IridiumSkyblock.netherOreUpgradeCache.get(oreLevel);
             else return;
 
             Bukkit.getScheduler().runTask(IridiumSkyblock.getInstance(), () -> {
+                if (!(toBlock.getType().equals(Material.COBBLESTONE) || toBlock.getType().equals(Material.STONE)))
+                    return;
                 final Random random = new Random();
                 final String oreUpgrade = islandOreUpgrades.get(random.nextInt(islandOreUpgrades.size()));
+
                 final XMaterial oreUpgradeXmaterial = XMaterial.valueOf(oreUpgrade);
                 final Material oreUpgradeMaterial = oreUpgradeXmaterial.parseMaterial(true);
                 if (oreUpgradeMaterial == null) return;
 
-                block.setType(oreUpgradeMaterial);
+                toBlock.setType(oreUpgradeMaterial);
 
-                final BlockState blockState = block.getState();
+                final BlockState blockState = toBlock.getState();
                 blockState.update(true);
 
-                if (Utils.isBlockValuable(block)) {
+                if (Utils.isBlockValuable(toBlock)) {
                     final XMaterial xmaterial = XMaterial.matchXMaterial(material);
                     island.valuableBlocks.compute(xmaterial.name(), (name, original) -> {
                         if (original == null) return 1;
@@ -128,21 +123,21 @@ public class BlockFromToListener implements Listener {
         final int y = location.getBlockY();
         final int z = location.getBlockZ();
         final int[][] coords = {
-            // At the same elevation
-            {x + 1, y, z},
-            {x - 1, y, z},
-            {x, y, z + 1},
-            {x, y, z - 1},
-            // Above
-            {x + 1, y + 1, z},
-            {x - 1, y + 1, z},
-            {x, y + 1, z + 1},
-            {x, y + 1, z - 1},
-            // Below
-            {x + 1, y - 1, z},
-            {x - 1, y - 1, z},
-            {x, y - 1, z + 1},
-            {x, y - 1, z - 1}
+                // At the same elevation
+                {x + 1, y, z},
+                {x - 1, y, z},
+                {x, y, z + 1},
+                {x, y, z - 1},
+                // Above
+                {x + 1, y + 1, z},
+                {x - 1, y + 1, z},
+                {x, y + 1, z + 1},
+                {x, y + 1, z - 1},
+                // Below
+                {x + 1, y - 1, z},
+                {x - 1, y - 1, z},
+                {x, y - 1, z + 1},
+                {x, y - 1, z - 1}
         };
         for (int[] coord : coords) {
             final Block block = world.getBlockAt(coord[0], coord[1], coord[2]);
