edcf92931866a520e3df8a6e983927d99058e0a6
Beech Horn <beechhorn@gmail.com>
2020-04-10 09:57:54 +0100

    Simplified onBlockBreak.

--- src/main/java/com/iridium/iridiumskyblock/listeners/onBlockBreak.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/onBlockBreak.java
@@ -1,67 +1,112 @@
 package com.iridium.iridiumskyblock.listeners;
 
-import com.iridium.iridiumskyblock.*;
 import com.iridium.iridiumskyblock.configs.Missions;
+import com.iridium.iridiumskyblock.IridiumSkyblock;
+import com.iridium.iridiumskyblock.Island;
+import com.iridium.iridiumskyblock.IslandManager;
+import com.iridium.iridiumskyblock.MissionType;
+import com.iridium.iridiumskyblock.User;
+import com.iridium.iridiumskyblock.Utils;
+import com.iridium.iridiumskyblock.XMaterial;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.block.CreatureSpawner;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.material.Crops;
 
+import java.util.List;
+import java.util.Map;
+
 public class onBlockBreak implements Listener {
 
     @EventHandler
-    public void onBreakBlock(BlockBreakEvent e) {
+    public void onBreakBlock(BlockBreakEvent event) {
         try {
-            if (e.isCancelled()) return;
-            User u = User.getUser(e.getPlayer());
-            Island island = IridiumSkyblock.getIslandManager().getIslandViaLocation(e.getBlock().getLocation());
-            if (island != null) {
-                if (u.islandID == island.getId()) {
-                    for (Missions.Mission mission : IridiumSkyblock.getMissions().missions) {
-                        if (!island.getMissionLevels().containsKey(mission.name))
-                            island.getMissionLevels().put(mission.name, 1);
-                        if (mission.levels.containsKey(island.getMissionLevels().get(mission.name))) {
-                            if (mission.levels.get(island.getMissionLevels().get(mission.name)).type == MissionType.BLOCK_BREAK) {
-                                if (mission.levels.get(island.getMissionLevels().get(mission.name)).conditions.isEmpty() || mission.levels.get(island.getMissionLevels().get(mission.name)).conditions.contains(XMaterial.matchXMaterial(e.getBlock().getType()).name()) || (e.getBlock().getState().getData() instanceof Crops &&
-                                        mission.levels.get(island.getMissionLevels().get(mission.name)).conditions.contains(((Crops) e.getBlock().getState().getData()).getState().toString()))) {
-                                    island.addMission(mission.name, 1);
-                                }
-                            }
-                        }
-                    }
-                }
-                if ((!island.getPermissions((u.islandID == island.getId() || island.isCoop(u.getIsland())) ? (island.isCoop(u.getIsland()) ? Role.Member : u.getRole()) : Role.Visitor).breakBlocks) && !u.bypassing)
-                    e.setCancelled(true);
-            } else {
-                if (e.getBlock().getLocation().getWorld().equals(IridiumSkyblock.getIslandManager().getWorld()) || e.getBlock().getLocation().getWorld().equals(IridiumSkyblock.getIslandManager().getNetherWorld())) {
-                    if (!u.bypassing) {
-                        e.setCancelled(true);
-                    }
+            if (event.isCancelled()) return;
+            final Player player = event.getPlayer();
+            final User user = User.getUser(player);
+            final boolean userBypassing = user.bypassing;
+            final Block block = event.getBlock();
+            final Location location = block.getLocation();
+            final World world = location.getWorld();
+            final IslandManager islandManager = IridiumSkyblock.getIslandManager();
+            final Island island = islandManager.getIslandViaLocation(location);
+
+            if (island == null) {
+                if (userBypassing) return;
+                if (world == null) return;
+                final String worldName = world.getName();
+                final World islandWorld = islandManager.getWorld();
+                if (islandWorld == null) return;
+                final World islandNetherWorld = islandManager.getNetherWorld();
+                if (islandNetherWorld == null) return;
+                if (islandWorld.getName().equals(worldName) || islandNetherWorld.getName().equals(worldName))
+                    event.setCancelled(true);
+                return;
+            }
+
+            if (user.islandID == island.getId()) {
+                for (Missions.Mission mission : IridiumSkyblock.getMissions().missions) {
+                    final int key = island.getMissionLevels().computeIfAbsent(mission.name, (name) -> 1);
+                    final Map<Integer, Missions.MissionData> levels = mission.levels;
+                    final Missions.MissionData level = levels.get(key);
+
+                    if (level == null) continue;
+                    if (level.type != MissionType.BLOCK_BREAK) continue;
+
+                    final List<String> conditions = level.conditions;
+
+                    if (
+                            conditions.isEmpty()
+                            ||
+                            conditions.contains(XMaterial.matchXMaterial(block.getType()).name())
+                            ||
+                            (
+                                    block.getState().getData() instanceof Crops
+                                    &&
+                                    conditions.contains(((Crops) block.getState().getData()).getState().toString())
+                            )
+                    ) island.addMission(mission.name, 1);
                 }
             }
-        } catch (Exception ex) {
-            IridiumSkyblock.getInstance().sendErrorMessage(ex);
+
+            if (!(island.getPermissions(user).breakBlocks || user.bypassing)) event.setCancelled(true);
+        } catch (Exception e) {
+            IridiumSkyblock.getInstance().sendErrorMessage(e);
         }
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void onMonitorBreakBlock(BlockBreakEvent e) {
-        Island island = IridiumSkyblock.getIslandManager().getIslandViaLocation(e.getBlock().getLocation());
-        if (island != null) {
-            if (Utils.isBlockValuable(e.getBlock())) {
-                if (!(e.getBlock().getState() instanceof CreatureSpawner)) {
-                    if (island.valuableBlocks.containsKey(XMaterial.matchXMaterial(e.getBlock().getType()).name())) {
-                        island.valuableBlocks.put(XMaterial.matchXMaterial(e.getBlock().getType()).name(), island.valuableBlocks.get(XMaterial.matchXMaterial(e.getBlock().getType()).name()) - 1);
+    public void onMonitorBreakBlock(BlockBreakEvent event) {
+        try {
+            final Block block = event.getBlock();
+            final Location location = block.getLocation();
+            final IslandManager islandManager = IridiumSkyblock.getIslandManager();
+            final Island island = islandManager.getIslandViaLocation(location);
+            if (island == null) return;
+
+            if (Utils.isBlockValuable(block)) {
+                if (!(block.getState() instanceof CreatureSpawner)) {
+                    final Material material = block.getType();
+                    final String materialName = XMaterial.matchXMaterial(material).name();
+                    if (island.valuableBlocks.containsKey(materialName)) {
+                        island.valuableBlocks.put(materialName, island.valuableBlocks.get(materialName) - 1);
                     }
-                    if (island.updating) {
-                        island.tempValues.remove(e.getBlock().getLocation());
-                    }
+                    if (island.updating)
+                        island.tempValues.remove(location);
                     island.calculateIslandValue();
                 }
             }
-            island.failedGenerators.remove(e.getBlock().getLocation());
+
+            island.failedGenerators.remove(location);
+        } catch (Exception e) {
+            IridiumSkyblock.getInstance().sendErrorMessage(e);
         }
     }
 }
