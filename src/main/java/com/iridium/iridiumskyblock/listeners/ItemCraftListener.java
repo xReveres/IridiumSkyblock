6cd0e8ad34ba8ce0f676c4b475b9619576bffd2c
Beech Horn <beechhorn@gmail.com>
2020-04-10 16:40:35 +0100

    Simplified onItemCraft.

---
+++ src/main/java/com/iridium/iridiumskyblock/listeners/ItemCraftListener.java
@@ -0,0 +1,24 @@
+package com.iridium.iridiumskyblock.listeners;
+
+import com.iridium.iridiumskyblock.IridiumSkyblock;
+import com.iridium.iridiumskyblock.Utils;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
+import org.bukkit.inventory.CraftingInventory;
+import org.bukkit.inventory.ItemStack;
+
+public class ItemCraftListener implements Listener {
+
+    @EventHandler
+    public void onItemCraft(PrepareItemCraftEvent event) {
+        final CraftingInventory inventory = event.getInventory();
+        if (inventory.getResult() == null) return;
+
+        for (ItemStack itemStack : inventory.getContents()) {
+            if (!Utils.makeItemHidden(IridiumSkyblock.getInventories().crystal).isSimilar(itemStack)) continue;
+            inventory.setResult(null);
+            return;
+        }
+    }
+}
