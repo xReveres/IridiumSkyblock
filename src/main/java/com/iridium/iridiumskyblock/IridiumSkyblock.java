153f279ca05969cfd545e9bad9e3c279ca8e4c3b
Beech Horn <beechhorn@gmail.com>
2020-04-10 18:45:23 +0100

    Simplified onSpawnerSpawn.

--- src/main/java/com/iridium/iridiumskyblock/IridiumSkyblock.java
+++ src/main/java/com/iridium/iridiumskyblock/IridiumSkyblock.java
@@ -190,7 +190,7 @@ public class IridiumSkyblock extends JavaPlugin {
                 shopGUI = new ShopGUI();
                 visitGUI = new HashMap<>();
 
-                registerListeners(new EntitySpawnListener(), new LeafDecayListener(), new BlockPistonListener(), new EntityPickupItemListener(), new PlayerTalkListener(), new ItemCraftListener(), new PlayerTeleportListener(), new PlayerPortalListener(), new BlockBreakListener(), new BlockPlaceListener(), new PlayerInteractListener(), new BlockFromToListener(), new onSpawnerSpawn(), new EntityDeathListener(), new PlayerJoinLeaveListener(), new BlockGrowListener(), new PlayerTalkListener(), new PlayerMoveListener(), new EntityDamageByEntityListener(), new PlayerExpChangeListener(), new PlayerFishListener(), new EntityExplodeListener());
+                registerListeners(new EntitySpawnListener(), new LeafDecayListener(), new BlockPistonListener(), new EntityPickupItemListener(), new PlayerTalkListener(), new ItemCraftListener(), new PlayerTeleportListener(), new PlayerPortalListener(), new BlockBreakListener(), new BlockPlaceListener(), new PlayerInteractListener(), new BlockFromToListener(), new SpawnerSpawnListener(), new EntityDeathListener(), new PlayerJoinLeaveListener(), new BlockGrowListener(), new PlayerTalkListener(), new PlayerMoveListener(), new EntityDamageByEntityListener(), new PlayerExpChangeListener(), new PlayerFishListener(), new EntityExplodeListener());
 
                 Bukkit.getScheduler().scheduleAsyncRepeatingTask(IridiumSkyblock.getInstance(), this::saveIslandManager, 0, 20 * 60);
 
---
+++ src/main/java/com/iridium/iridiumskyblock/listeners/SpawnerSpawnListener.java
@@ -0,0 +1,31 @@
+package com.iridium.iridiumskyblock.listeners;
+
+import com.iridium.iridiumskyblock.IridiumSkyblock;
+import com.iridium.iridiumskyblock.Island;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.SpawnerSpawnEvent;
+
+public class SpawnerSpawnListener implements Listener {
+
+    @EventHandler
+    public void onSpawnerSpawn(SpawnerSpawnEvent event) {
+        try {
+            final Location location= event.getLocation();
+            final Island island = IridiumSkyblock.getIslandManager().getIslandViaLocation(location);
+            if (island == null) return;
+
+            if (island.getSpawnerBooster() == 0) return;
+
+            final IridiumSkyblock plugin = IridiumSkyblock.getInstance();
+            final CreatureSpawner spawner = event.getSpawner();
+            Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> spawner.setDelay(spawner.getDelay() / 2), 0);
+        } catch (Exception e) {
+            IridiumSkyblock.getInstance().sendErrorMessage(e);
+        }
+    }
+
+}
