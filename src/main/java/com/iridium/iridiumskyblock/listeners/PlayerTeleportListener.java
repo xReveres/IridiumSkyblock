92bec7f7ba0c9edb568934854bafcd4b1fce2c32
Peaches_MLG <PeachesMLG100@gmail.com>
2020-07-01 14:12:49 +0100

    Fixed Issues with visiting islands

--- src/main/java/com/iridium/iridiumskyblock/listeners/PlayerTeleportListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/PlayerTeleportListener.java
@@ -24,13 +24,15 @@ public class PlayerTeleportListener implements Listener {
             if (user.islandID == island.getId()) return;
 
             if ((island.isVisit() && !island.isBanned(user)) || user.bypassing) {
-                Bukkit.getScheduler().scheduleSyncDelayedTask(IridiumSkyblock.getInstance(), () -> island.sendBorder(player), 1);
-                if (user.islandID != island.getId()) {
-                    player.sendMessage(Utils.color(IridiumSkyblock.getMessages().visitingIsland.replace("%player%", User.getUser(island.getOwner()).name).replace("%prefix%", IridiumSkyblock.getConfiguration().prefix)));
-                    for (String pl : island.getMembers()) {
-                        Player p = Bukkit.getPlayer(User.getUser(pl).name);
-                        if (p != null) {
-                            p.sendMessage(Utils.color(IridiumSkyblock.getMessages().visitedYourIsland.replace("%player%", player.getName()).replace("%prefix%", IridiumSkyblock.getConfiguration().prefix)));
+                if (island.isInIsland(event.getFrom())) {
+                    Bukkit.getScheduler().scheduleSyncDelayedTask(IridiumSkyblock.getInstance(), () -> island.sendBorder(player), 1);
+                    if (user.islandID != island.getId()) {
+                        player.sendMessage(Utils.color(IridiumSkyblock.getMessages().visitingIsland.replace("%player%", User.getUser(island.getOwner()).name).replace("%prefix%", IridiumSkyblock.getConfiguration().prefix)));
+                        for (String pl : island.getMembers()) {
+                            Player p = Bukkit.getPlayer(User.getUser(pl).name);
+                            if (p != null && p.canSee(player)) {
+                                p.sendMessage(Utils.color(IridiumSkyblock.getMessages().visitedYourIsland.replace("%player%", player.getName()).replace("%prefix%", IridiumSkyblock.getConfiguration().prefix)));
+                            }
                         }
                     }
                 }
