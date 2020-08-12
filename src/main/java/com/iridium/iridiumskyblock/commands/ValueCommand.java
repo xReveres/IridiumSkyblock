a1e1a57b01fce3dee8adf77d9644c37f68262f19
Peaches_MLG <PeachesMLG100@gmail.com>
2020-08-12 18:16:11 +0100

    Made the plugin recalculate on /is value

--- src/main/java/com/iridium/iridiumskyblock/commands/ValueCommand.java
+++ src/main/java/com/iridium/iridiumskyblock/commands/ValueCommand.java
@@ -30,7 +30,10 @@ public class ValueCommand extends Command {
             island = user.getIsland();
         }
         if (island != null) {
-            p.sendMessage(Utils.color(IridiumSkyblock.getMessages().islandValue.replace("%value%", island.getValue() + "").replace("%rank%", Utils.getIslandRank(island) + "").replace("%prefix%", IridiumSkyblock.getConfiguration().prefix)));
+            Bukkit.getScheduler().runTaskAsynchronously(IridiumSkyblock.getInstance(), () -> {
+                island.calculateIslandValue();
+                p.sendMessage(Utils.color(IridiumSkyblock.getMessages().islandValue.replace("%value%", island.getValue() + "").replace("%rank%", Utils.getIslandRank(island) + "").replace("%prefix%", IridiumSkyblock.getConfiguration().prefix)));
+            });
         } else {
             p.sendMessage(Utils.color(IridiumSkyblock.getMessages().noIsland.replace("%prefix%", IridiumSkyblock.getConfiguration().prefix)));
         }
