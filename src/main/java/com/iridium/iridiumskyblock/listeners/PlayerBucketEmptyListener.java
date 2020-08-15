522a909c52b1aad1514a837e1fc70dca9e4aaef0
iceBear67 <48877375+iceBear67@users.noreply.github.com>
2020-08-15 12:46:35 +0800

    Fix PlayerBucketEmptyListener in paper1.13

--- src/main/java/com/iridium/iridiumskyblock/listeners/PlayerBucketEmptyListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/PlayerBucketEmptyListener.java
@@ -8,6 +8,7 @@ import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
@@ -25,7 +26,8 @@ public class PlayerBucketEmptyListener implements Listener {
         final Material type = event.getBucket();
         final ItemStack item = event.getPlayer().getItemInHand();
         Block block;
-        if (supports) {
+        final boolean isInPaper113 = Bukkit.geVersion().equals("Paper") && Bukkit.getBukkitVersion().contains("1.13");
+        if (supports && !isInPaper113) {
             block = event.getBlock();
         } else {
             try {
