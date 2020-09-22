c1eb59632dd08aaf622a528a86782dfcd201ca89
Nik2143 <44278678+nik2143@users.noreply.github.com>
2020-09-22 23:08:47 +0200

    Changed MissionCompleteEvent to extend IslandEvent

--- src/main/java/com/iridium/iridiumskyblock/api/MissionCompleteEvent.java
+++ src/main/java/com/iridium/iridiumskyblock/api/MissionCompleteEvent.java
@@ -1,22 +1,20 @@
 package com.iridium.iridiumskyblock.api;
 
+import com.iridium.iridiumskyblock.Island;
 import com.iridium.iridiumskyblock.MissionType;
+import com.iridium.iridiumskyblock.User;
 import lombok.Getter;
-import org.bukkit.event.Event;
-import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
-public class MissionCompleteEvent extends Event {
-    @NotNull private static final HandlerList handlers = new HandlerList();
-    @NotNull public HandlerList getHandlers() { return handlers; }
-    @NotNull public static HandlerList getHandlerList() {
-        return handlers;
-    }
+public class MissionCompleteEvent extends IslandEvent {
     @NotNull @Getter String missionName;
     @NotNull @Getter MissionType missionType;
+    @NotNull @Getter User usercompleted;
     @Getter int missionLevel;
 
-    public MissionCompleteEvent(@NotNull String missionName, @NotNull MissionType missionType, int missionLevel) {
+    public MissionCompleteEvent(@NotNull Island island, @NotNull String missionName, @NotNull MissionType missionType, int missionLevel) {
+        super(island);
+        this.usercompleted = usercompleted;
         this.missionName = missionName;
         this.missionType = missionType;
         this.missionLevel = missionLevel;
