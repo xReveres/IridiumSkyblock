b664d40db977e0a09ca41fd2b1344bd0ad589572
Peaches_MLG <PeachesMLG100@gmail.com>
2020-11-22 15:21:12 +0000

    Fixed issues with trees growing outside the border

---
+++ src/main/java/com/iridium/iridiumskyblock/listeners/StructureGrowListener.java
@@ -0,0 +1,23 @@
+package com.iridium.iridiumskyblock.listeners;
+
+import com.iridium.iridiumskyblock.IridiumSkyblock;
+import com.iridium.iridiumskyblock.Island;
+import org.bukkit.Material;
+import org.bukkit.block.BlockState;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.world.StructureGrowEvent;
+
+public class StructureGrowListener implements Listener {
+
+    @EventHandler
+    public void onStructureGrowEvent(StructureGrowEvent event) {
+        Island island = IridiumSkyblock.getIslandManager().getIslandViaLocation(event.getLocation());
+        for (BlockState blockState : event.getBlocks()) {
+            if (!island.isInIsland(blockState.getLocation())) {
+                blockState.setType(Material.AIR);
+            }
+        }
+    }
+
+}
