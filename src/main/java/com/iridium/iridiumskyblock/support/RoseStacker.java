a8b446276771a84ebd0bb3146a4c7da3c3d98628
Peaches_MLG <PeachesMLG100@gmail.com>
2020-11-29 13:21:06 +0000

    Fixed NPE with RoseStacker

--- src/main/java/com/iridium/iridiumskyblock/support/RoseStacker.java
+++ src/main/java/com/iridium/iridiumskyblock/support/RoseStacker.java
@@ -2,6 +2,7 @@ package com.iridium.iridiumskyblock.support;
 
 import com.iridium.iridiumskyblock.IridiumSkyblock;
 import dev.rosewood.rosestacker.api.RoseStackerAPI;
+import dev.rosewood.rosestacker.stack.StackedSpawner;
 import org.bukkit.block.CreatureSpawner;
 
 public class RoseStacker implements SpawnerSupport{
@@ -11,6 +12,8 @@ public class RoseStacker implements SpawnerSupport{
     }
 
     public int getSpawnerAmount(CreatureSpawner spawner) {
-        return RoseStackerAPI.getInstance().getStackedSpawner(spawner.getBlock()).getStackSize();
+        StackedSpawner stackedSpawner = RoseStackerAPI.getInstance().getStackedSpawner(spawner.getBlock());
+        if(stackedSpawner==null)return 1;
+        return stackedSpawner.getStackSize();
     }
 }
