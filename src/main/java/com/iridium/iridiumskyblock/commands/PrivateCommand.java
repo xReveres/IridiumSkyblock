cfb7db26b234cffad4d8e63a47df47f6456cc7e5
Peaches_MLG <PeachesMLG100@gmail.com>
2021-01-13 11:42:16 +0000

    Fixed issues with /is private

--- src/main/java/com/iridium/iridiumskyblock/commands/PrivateCommand.java
+++ src/main/java/com/iridium/iridiumskyblock/commands/PrivateCommand.java
@@ -23,20 +23,14 @@ public class PrivateCommand extends Command {
         if (user.getIsland() != null) {
             if (user.bypassing || user.getIsland().getPermissions(user.role).islandprivate) {
                 user.getIsland().visit = false;
-                int visitorCount = 0;
-                for (Player visitor : user.getIsland().getPlayersOnIsland()) {
-                    if (user.getIsland().equals(User.getUser(visitor).getIsland()) || User.getUser(visitor).bypassing || user.getIsland().isCoop(User.getUser(visitor).getIsland()) || visitor.hasPermission("iridiumskyblock.visitbypass"))
-                        continue;
-
-                    user.getIsland().spawnPlayer(visitor);
-                    visitor.sendMessage(Utils.color(IridiumSkyblock.getMessages().expelledIslandLocked
+                user.getIsland().getPlayersOnIsland().stream().filter(player -> user.getIsland().id != User.getUser(player).getIsland().id && !User.getUser(player).bypassing && !user.getIsland().isCoop(User.getUser(player).getIsland()) && !player.hasPermission("iridiumskyblock.visitbypass")).forEach(player -> {
+                    user.getIsland().spawnPlayer(player);
+                    player.sendMessage(Utils.color(IridiumSkyblock.getMessages().expelledIslandLocked
                             .replace("%prefix%", IridiumSkyblock.getConfiguration().prefix)
                             .replace("%player%", p.getName())));
-                    visitorCount++;
-                }
+                });
                 p.sendMessage(Utils.color(IridiumSkyblock.getMessages().islandNowPrivate
-                        .replace("%prefix%", IridiumSkyblock.getConfiguration().prefix)
-                        .replace("%amount%", visitorCount + "")));
+                        .replace("%prefix%", IridiumSkyblock.getConfiguration().prefix)));
             } else {
                 sender.sendMessage(Utils.color(IridiumSkyblock.getMessages().noPermission.replace("%prefix%", IridiumSkyblock.getConfiguration().prefix)));
             }
