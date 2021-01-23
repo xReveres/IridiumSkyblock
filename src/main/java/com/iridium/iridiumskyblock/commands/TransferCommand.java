46cc76e008646330a23abc47ddde821021f47cea
MertUnverdi <69310169+MertUnverdi@users.noreply.github.com>
2021-01-23 23:55:43 +0300

    LeaderChangeEvent for api
    our api is not rich, we could add more events :+1:

--- src/main/java/com/iridium/iridiumskyblock/commands/TransferCommand.java
+++ src/main/java/com/iridium/iridiumskyblock/commands/TransferCommand.java
@@ -2,7 +2,9 @@ package com.iridium.iridiumskyblock.commands;
 
 import com.iridium.iridiumskyblock.IridiumSkyblock;
 import com.iridium.iridiumskyblock.Island;
+import com.iridium.iridiumskyblock.Role;
 import com.iridium.iridiumskyblock.User;
+import com.iridium.iridiumskyblock.api.IslandLeaderChangeEvent;
 import com.iridium.iridiumskyblock.gui.ConfirmationGUI;
 import com.iridium.iridiumskyblock.utils.StringUtils;
 import org.bukkit.Bukkit;
@@ -31,8 +33,13 @@ public class TransferCommand extends Command {
             Island island = user.getIsland();
             if (island.owner.equals(p.getUniqueId().toString())) {
                 OfflinePlayer player = Bukkit.getOfflinePlayer(args[1]);
-                if (User.getUser(player).getIsland() == island) {
-                    p.openInventory(new ConfirmationGUI(user.getIsland(), () -> island.setOwner(player), IridiumSkyblock.getInstance().getMessages().transferAction.replace("%player%", player.getName())).getInventory());
+                User toUser = User.getUser(player);
+                if (island.equals(toUser.getIsland())) {
+                    IslandLeaderChangeEvent event = new IslandLeaderChangeEvent(island, user, toUser);
+                    Bukkit.getPluginManager().callEvent(event);
+                    if (!event.isCancelled()) {
+                        p.openInventory(new ConfirmationGUI(user.getIsland(), () -> island.setOwner(player), IridiumSkyblock.getInstance().getMessages().transferAction.replace("%player%", toUser.name)).getInventory());
+                    }
                 } else {
                     sender.sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getMessages().notInYourIsland.replace("%prefix%", IridiumSkyblock.getInstance().getConfiguration().prefix)));
                 }
