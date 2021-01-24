0a3196ebfe9185a7427991cfc207e98cb37e33e4
Peaches_MLG <PeachesMLG100@gmail.com>
2021-01-24 20:28:22 +0000

    My attempt at making stuff slightly more readable

--- src/main/java/com/iridium/iridiumskyblock/configs/Schematics.java
+++ src/main/java/com/iridium/iridiumskyblock/configs/Schematics.java
@@ -9,9 +9,18 @@ import java.util.List;
 
 public class Schematics {
     public List<FakeSchematic> schematicList = Arrays.asList(
-            new FakeSchematic(0, 0, 0, new SchematicData("desert.schem", XBiome.DESERT, 0.0, 0.0, 0.0), new SchematicData("nether.schematic", XBiome.NETHER_WASTES, 0.0, 0.0, 0.0), "", new Inventories.Item(XMaterial.PLAYER_HEAD, 11, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNGY0OTNkZDgwNjUzM2Q5ZDIwZTg0OTUzOTU0MzY1ZjRkMzY5NzA5Y2ViYzlkZGVmMDIyZDFmZDQwZDg2YTY4ZiJ9fX0=", 1, "&b&lDesert Island", Collections.singletonList("&7A starter desert island."))),
-            new FakeSchematic(0, 0, 0, new SchematicData("jungle.schem", XBiome.JUNGLE, 0.0, 0.0, 0.0), new SchematicData("nether.schematic", XBiome.NETHER_WASTES, 0.0, 0.0, 0.0), "", new Inventories.Item(XMaterial.PLAYER_HEAD, 13, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNjgzYWRmNDU2MGRlNDc0MTQwNDA5M2FjNjFjMzNmYjU1NmIzZDllZTUxNDBmNjIwMzYyNTg5ZmRkZWRlZmEyZCJ9fX0=", 1, "&b&lJungle Island", Collections.singletonList("&7A starter jungle island."))),
-            new FakeSchematic(0, 0, 0, new SchematicData("mushroom.schem", XBiome.MUSHROOM_FIELDS, 0.0, 0.0, 0.0), new SchematicData("nether.schematic", XBiome.NETHER_WASTES, 0.0, 0.0, 0.0), "", new Inventories.Item(XMaterial.PLAYER_HEAD, 15, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZWE0NWQxYjQxN2NiZGRjMjE3NjdiMDYwNDRlODk5YjI2NmJmNzhhNjZlMjE4NzZiZTNjMDUxNWFiNTVkNzEifX19", 1, "&b&lMushroom Island", Collections.singletonList("&7A starter mushroom island.")))
+            new FakeSchematic(0, 0, 0,
+                    new SchematicData("desert.schem", XBiome.DESERT, 0.0, 0.0, 0.0),
+                    new SchematicData("nether.schematic", XBiome.NETHER_WASTES, 0.0, 0.0, 0.0), "",
+                    new Inventories.Item(XMaterial.PLAYER_HEAD, 11, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNGY0OTNkZDgwNjUzM2Q5ZDIwZTg0OTUzOTU0MzY1ZjRkMzY5NzA5Y2ViYzlkZGVmMDIyZDFmZDQwZDg2YTY4ZiJ9fX0=", 1, "&b&lDesert Island", Collections.singletonList("&7A starter desert island."))),
+            new FakeSchematic(0, 0, 0,
+                    new SchematicData("jungle.schem", XBiome.JUNGLE, 0.0, 0.0, 0.0),
+                    new SchematicData("nether.schematic", XBiome.NETHER_WASTES, 0.0, 0.0, 0.0), "",
+                    new Inventories.Item(XMaterial.PLAYER_HEAD, 13, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNjgzYWRmNDU2MGRlNDc0MTQwNDA5M2FjNjFjMzNmYjU1NmIzZDllZTUxNDBmNjIwMzYyNTg5ZmRkZWRlZmEyZCJ9fX0=", 1, "&b&lJungle Island", Collections.singletonList("&7A starter jungle island."))),
+            new FakeSchematic(0, 0, 0,
+                    new SchematicData("mushroom.schem", XBiome.MUSHROOM_FIELDS, 0.0, 0.0, 0.0),
+                    new SchematicData("nether.schematic", XBiome.NETHER_WASTES, 0.0, 0.0, 0.0), "",
+                    new Inventories.Item(XMaterial.PLAYER_HEAD, 15, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZWE0NWQxYjQxN2NiZGRjMjE3NjdiMDYwNDRlODk5YjI2NmJmNzhhNjZlMjE4NzZiZTNjMDUxNWFiNTVkNzEifX19", 1, "&b&lMushroom Island", Collections.singletonList("&7A starter mushroom island.")))
     );
 
     public static class FakeSchematic {
